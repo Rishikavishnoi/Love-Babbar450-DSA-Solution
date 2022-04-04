e4a692d5684e4dfac92607ad75e1d10d77e01261
Rishika <rishikavishnoi56@gmail.com>
2022-04-05 04:21:58 +0530

    stack problem solution

---
+++ Stack/PostfixExpression.java
@@ -0,0 +1,33 @@
+package dsaSheet450;
+import java.util.*;
+public class PostfixExpression {
+
+	public static void main(String[] args) {
+		String s="231*+9-";
+		int ans=evaluatePostFix( s);
+    System.out.println(ans);
+	}
+
+	private static int evaluatePostFix(String s) {
+		Stack<Integer> st =new Stack<>();
+		for(int i=0;i<s.length();i++) {
+			int ch=s.charAt(i);
+			if(ch>='0'&& ch<='9') {
+				st.push(ch-'0');
+			}
+			else {
+				
+					int top1=st.pop();
+					int top2=st.pop();
+					 if(ch=='+') st.push( top2+top1);
+					 else if (ch=='*') st.push(top2*top1);
+					 else if(ch=='-') st.push(top2-top1);
+					 else if(ch=='/')st.push(top2/top1);
+				
+			}
+		}
+		
+		return st.pop();
+	}
+
+}
---
+++ Stack/StackUsingArray.java
@@ -0,0 +1,10 @@
+package dsaSheet450;
+
+public class StackUsingArray {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
